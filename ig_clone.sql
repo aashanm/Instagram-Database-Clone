9f29fb6f3d4c8eeb3f34ef241320c40786684b98
Aashan M <43193308+aashanm@users.noreply.github.com>
2020-03-07 16:55:06 -0500

    Removed testing insert statements

--- ig_clone.sql
+++ ig_clone.sql
@@ -59,41 +59,64 @@ CREATE TABLE photo_tags (
 	PRIMARY KEY (photo_id, tag_id)
 );
 
-INSERT INTO users (username) VALUES
-('Aashan'),
-('Bob'),
-('Charly');
+DROP DATABASE ig_clone;
+CREATE DATABASE ig_clone;
+USE ig_clone;
 
-INSERT INTO photos (image_url, user_id) VALUES
-('/weiubw', 1),
-('/oigen', 2),
-('/erinoe', 2);
+CREATE TABLE users (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW()
+);
 
-INSERT INTO comments(comment_text, user_id, photo_id) VALUES
-('Hello!', 1, 2),
-('Cool!', 3, 2),
-('This is sick', 2, 1);
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
-INSERT INTO likes(user_id, photo_id) VALUES 
-(1,1),
-(2,1),
-(1,2),
-(1,3),
-(3,3);
+CREATE TABLE comments (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	comment_text VARCHAR(255) NOT NULL,
+	user_id INTEGER NOT NULL,
+	photo_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
 
-INSERT INTO follows(follower_id, followee_id) VALUES
-(1,2),
-(1,3),
-(3,1),
-(2,3);
+CREATE TABLE likes (
+	user_id INTEGER NOT NULL,
+	photo_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	-- allows for only 1 like per user per photo
+	PRIMARY KEY(user_id, photo_id)
+);
 
-INSERT INTO tags(tag_name) VALUES
-('adorable'),
-('cute'),
-('sunrise');
+CREATE TABLE follows (
+	follower_id INTEGER NOT NULL,
+	followee_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(follower_id) REFERENCES users(id),
+	FOREIGN KEY(followee_id) REFERENCES users(id),
+	PRIMARY KEY(follower_id, followee_id)
+);
 
-INSERT INTO photo_tags(photo_id, tag_id) VALUES
-(1,1),
-(1,2),
-(2,3),
-(3,2);
+CREATE TABLE tags (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	tag_name VARCHAR(255) UNIQUE,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+	photo_id INTEGER NOT NULL,
+	tag_id INTEGER NOT NULL,
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	FOREIGN KEY(tag_id) REFERENCES tags(id),
+	PRIMARY KEY (photo_id, tag_id)
+);
+
