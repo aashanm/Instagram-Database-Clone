515711b26c7b09dc5b4c64f35021aa4f0b2743e8
Aashan M <43193308+aashanm@users.noreply.github.com>
2020-03-07 16:35:00 -0500

    Create likes schema

--- ig_clone.sql
+++ ig_clone.sql
@@ -26,6 +26,16 @@ CREATE TABLE comments (
 	FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
 
+CREATE TABLE likes (
+	user_id INTEGER NOT NULL,
+	photo_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	-- allows for only 1 like per user per photo
+	PRIMARY KEY(user_id, photo_id)
+);
+
 INSERT INTO users (username) VALUES
 ('Aashan'),
 ('Bob'),
@@ -41,7 +51,9 @@ INSERT INTO comments(comment_text, user_id, photo_id) VALUES
 ('Cool!', 3, 2),
 ('This is sick', 2, 1);
 
-SELECT photos.image_url, users.username
-FROM photos
-JOIN users
-	ON photos.user_id = users.id;
+INSERT INTO likes(user_id, photo_id) VALUES 
+(1,1),
+(2,1),
+(1,2),
+(1,3),
+(3,3);
